62f4d827e026b510f7152baaf4cef62f13e9ebce
Aman Verma <av580731@gmail.com>
2026-08-16 23:15:05 +0530

    first commit

---
+++ set_0_matrix.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix ) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int count=0,count2=0;
+        for (int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if (matrix[0][j]==0){
+                    count++;
+                    break;
+                }
+            }
+        }
+          for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (matrix[i][0]==0){
+                    count2++;
+                    break;
+                }
+            }
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        for (int i=1;i<n;i++){
+            for (int j=1;j<m;j++){
+                if (matrix[i][j] !=0){
+                    if(matrix[0][j]==0 || matrix[i][0]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (count!=0){
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (count2!=0){
+                    matrix[i][0]=0;
+                }
+            }
+        }
+    }
+};
